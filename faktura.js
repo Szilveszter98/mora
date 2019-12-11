58101447df648b1652cd1278766ede0bcb6b70f3
Szilveszter98 <szilveszter.mag98@gmail.com>
2019-12-11 10:08:04 +0100

    pdf

--- faktura.js
+++ faktura.js
@@ -73,33 +73,15 @@ class Storage {
 
 
 
-// print js
-/* const printFaktura = document.querySelector("#printFaktura");
-const fakturaContent = document.querySelector(".faktura");
-printFaktura.addEventListener("click", () => { window.print(fakturaContent) });
- */
 
-/* Print Faktura*/
-function printDiv(printableArea) {
-    var printContents = document.getElementById(printableArea).innerHTML;
-    var originalContents = document.body.innerHTML;
-
-    document.body.innerHTML = printContents;
-
-    window.print();
-
-    document.body.innerHTML = originalContents;
-}
 /*Download direkt pdf*/ 
-document.querySelector(".btn-pdf").addEventListener("click", function (){
-    var doc = new jsPDF();
 
-doc.getElementById("printableArea", 10, 10)
-doc.viewerPreferences({'FitWindow': true}, true)
-doc.save("viewerPreferences.pdf")
-
-})
-
-window.onbeforeunload = function() {
-    localStorage.clear();
+ function getpdf() {
+    var pdf = new jsPDF('p', 'mm', "a4");
+    pdf.addHTML(document.body, function() {
+      pdf.save('web.pdf');
+      pdf
+    });
 }
+    
+document.getElementById("pdf").addEventListener("click", getpdf);
